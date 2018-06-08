f2bbe7234df8ee2f1ebb3bacfed5bd449ad1ceb5
rias-z <chestermx88@gmail.com>
2018-06-08 16:09:08 +0900

    feat: テスト終了時データベースを削除する

--- tests/base.py
+++ tests/base.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from app.config import create_dburl, current_config
 from tests.utils import (
     create_database,
+    drop_database,
     create_tables
 )
 
@@ -18,12 +19,22 @@ class AbstractTest(unittest.TestCase):
         cls.create_tables()
 
     @classmethod
+    def tearDownClass(cls):
+        # データベースの削除
+        cls.drop_database()
+
+    @classmethod
     def create_database(cls):
         '''テスト用データベース作成
         '''
         create_database()
 
     @classmethod
+    def drop_database(cls):
+        '''テスト用データベース削除
+        '''
+        drop_database()
+
+    @classmethod
     def create_tables(cls):
         create_tables()
-

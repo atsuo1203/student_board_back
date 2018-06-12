c2422f0c566f8e46ba66c8d5f691e350afb1838a
rias-z <chestermx88@gmail.com>
2018-06-12 19:40:52 +0900

    feat: commentのテストデータ読み込みの関数追加

--- tests/base.py
+++ tests/base.py
@@ -1,11 +1,13 @@
 import unittest
 
+from app.models.comment import create_comment_table, drop_comment_table
 from tests.utils import (
     drop_tables,
     create_database,
     create_tables,
     drop_database,
     load_fixtures,
+    load_comment_fixtures,
 )
 
 
@@ -16,6 +18,7 @@ class AbstractTest(unittest.TestCase):
     # 利用するテーブル
     # 外部キー制約による読み込み順番に注意
     tables = []
+    comment_tables = []
 
     @classmethod
     def setUpClass(cls):
@@ -62,3 +65,31 @@ class AbstractTest(unittest.TestCase):
         '''テストデータ読み込み
         '''
         load_fixtures(fixture_data_root, cls.tables)
+
+    @classmethod
+    def load_comment_fixtures(cls):
+        '''commentテストデータ読み込み
+        '''
+        load_comment_fixtures(fixture_data_root, cls.comment_tables)
+
+    @classmethod
+    def create_comment_tables(cls):
+        '''load_fixtures後，thread_idに紐づくcommentテーブル生成
+        '''
+        from app.models.thread import Thread
+
+        threads = Thread.get_all()
+
+        for thread in threads:
+            create_comment_table(thread.get('thread_id'))
+
+    @classmethod
+    def drop_comment_tables(cls):
+        '''tearDown実行前，commentテーブル削除
+        '''
+        from app.models.thread import Thread
+
+        threads = Thread.get_all()
+
+        for thread in threads:
+            drop_comment_table(thread.get('thread_id'))
--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -1,4 +1,7 @@
+import yaml
+
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy_seed import load_fixture_files as ss_load_fixture_files
 from sqlalchemy_seed import load_fixtures as ss_load_fixtures
 from sqlalchemy_utils import database_exists
@@ -42,7 +45,6 @@ def drop_database():
 def create_tables():
     '''全テーブル作成（commentテーブル以外を作成）
     '''
-
     db_modules = __import__(models_path, fromlist=[''])
 
     with session_scope() as session:
@@ -69,9 +71,41 @@ def load_fixtures(fixtures_root, tables):
 
     fixtures_data_path = ['%s.yaml' % table for table in tables]
 
-    # fixtures_dataを読み込み
-    fixtures_data = ss_load_fixture_files(fixtures_root, fixtures_data_path)
+    for table in fixtures_data_path:
+        # fixtures_dataを読み込み
+        fixture_data = ss_load_fixture_files(fixtures_root, [table])
 
-    with session_scope() as session:
-        # fixtures_dataをテーブルに格納
-        ss_load_fixtures(session, fixtures_data)
+        with session_scope() as session:
+            # fixtures_dataをテーブルに格納
+            ss_load_fixtures(session, fixture_data)
+
+
+def load_comment_fixtures(fixtures_root, tables):
+    '''commentテストデータ読み込み
+    '''
+    root_path = __file__[:__file__.rfind('tests')]
+
+    for table_name in tables:
+        path = '%s/%s/%s.yaml' % (root_path, fixtures_root, table_name)
+
+        with open(path, 'r') as f:
+            data_list = yaml.load(f)
+
+            with session_scope() as session:
+                for data in data_list:
+                    dburl = create_dburl()
+                    engine = create_engine(dburl)
+                    base = declarative_base(engine)
+
+                    comment_table_dic = {
+                        '__tablename__': table_name,
+                        '__table_args__': {'autoload': True}
+                    }
+                    comment_table = type(
+                        'comment_table', (base,), comment_table_dic
+                    )
+
+                    data = comment_table(
+                        **data
+                    )
+                    session.add(data)

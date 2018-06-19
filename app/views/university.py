6fb1e928e9664410492f6f6ae4cb57d41f306e14
rias-z <chestermx88@gmail.com>
2018-06-19 19:46:53 +0900

    feat|fix: views.university check_webtokenと簡易的なエラーハンドリング実装

--- app/views/university.py
+++ app/views/university.py
@@ -1,32 +1,45 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, make_response
 
 from app.models.university import University
+from app.views.utils.check_webtoken import check_webtoken
 
 
 app = Blueprint('university', __name__)
 
 
 @app.route('/university', methods=['GET'])
+@check_webtoken
 def get_all():
     '''すべてのuniversity情報取得
     Returns:
-        list(dict):
-            university情報(dict)のリスト
+        200:
+            list(dict):
+                university情報(dict)のリスト
+        500: サーバエラー
     '''
-    result = University.all()
+    try:
+        result = University.all()
 
-    return jsonify(result)
+        return jsonify(result)
+    except Exception as e:
+        return make_response('', 500)
 
 
 @app.route('/university/<university_id>', methods=['GET'])
+@check_webtoken
 def get(university_id):
     '''university_idに紐づくuniversity情報取得
     Args:
         university_id:  大学ID
     Returns:
-        list(dict):
-            university情報(dict)のリスト
+        200:
+            list(dict):
+                university情報(dict)のリスト
+        500: サーバエラー
     '''
-    result = University.get(university_id)
+    try:
+        result = University.get(university_id)
 
-    return jsonify(result)
+        return jsonify(result)
+    except Exception as e:
+        return make_response('', 500)
